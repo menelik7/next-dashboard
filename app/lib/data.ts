7903228d6896be2c33cb54fdd993d5f2b041596e
menelik7 <menelikworku@gmail.com>
2023-12-18 14:44:05 +0100

    Wrapping the Card component to create a more staggered effect instead of fetching data separately in page.tsx for each Card component and wrapping each in Suspense which would lead to a popping effect

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -21,7 +21,7 @@ export async function fetchRevenue() {
     // Don't do this in production :)
 
     console.log('Fetching revenue data...');
-    await new Promise((resolve) => setTimeout(resolve, 3000));
+    await new Promise((resolve) => setTimeout(resolve, 1000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
@@ -38,6 +38,7 @@ export async function fetchLatestInvoices() {
   noStore();
 
   try {
+    await new Promise((resolve) => setTimeout(resolve, 600));
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
@@ -60,6 +61,7 @@ export async function fetchCardData() {
   noStore();
 
   try {
+    await new Promise((resolve) => setTimeout(resolve, 300));
     // You can probably combine these into a single SQL query
     // However, we are intentionally splitting them to demonstrate
     // how to initialize multiple queries in parallel with JS.
